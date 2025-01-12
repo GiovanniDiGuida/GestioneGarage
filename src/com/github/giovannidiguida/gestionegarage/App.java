1df712e3f0b823df715e392eb13ab6d7633d7640
RedFox197 <190109902+RedFox197@users.noreply.github.com>
2025-01-13 00:28:08 +0100

    garage template per garage solo auto o solo moto o misto

--- src/com/github/giovannidiguida/gestionegarage/App.java
+++ src/com/github/giovannidiguida/gestionegarage/App.java
@@ -11,24 +11,30 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Benvenuto nel Garage");
 
-        Garage garage = new Garage();
-        garage.aggiungiVeicolo(new Moto("Modello Pippo", "Audi", "ABC123", 1900, 3000, 800));
-        garage.aggiungiVeicolo(new Moto("Modello Luca", "Idua", "CBA321", 1950, 4000, 100));
-        garage.aggiungiVeicolo(new Moto("Modello Bimbi", "Peppe", "Chicco", 2012, 4000, 610));
-        garage.aggiungiVeicolo(new Auto("Modello Catalin", "Dacia", "FRE123", 1998, 100, 4));
-        garage.aggiungiVeicolo(new Auto("Modello Giovanni", "Rubare", "IoRubo123", 1995, 100, 5));
+        Garage<Veicolo> garageMisto = new Garage<>();
+        garageMisto.aggiungiVeicolo(new Moto("Modello Pippo", "Audi", "ABC123", 1900, 3000, 800));
+        garageMisto.aggiungiVeicolo(new Moto("Modello Luca", "Idua", "CBA321", 1950, 4000, 100));
+        garageMisto.aggiungiVeicolo(new Moto("Modello Bimbi", "Peppe", "Chicco", 2012, 4000, 610));
+        garageMisto.aggiungiVeicolo(new Auto("Modello Catalin", "Dacia", "FRE123", 1998, 100, 4));
+        garageMisto.aggiungiVeicolo(new Auto("Modello Giovanni", "Rubare", "IoRubo123", 1995, 100, 5));
+
+        /*
+        Per creare garage specifici solo moto o auto
+        Garage<Moto> garageMoto = new Garage<>();
+        Garage<Auto> garageAuto = new Garage<>();
+         */
 
         //potevo usare anche il metodo garage.veicoliOrdinati, non lo uso solo per mostrare l'implementazione del Comparable<Veicolo>
         System.out.println("\nOrdinati per anno");
-        List<Veicolo> ordinatiPerAnno = garage.tuttiVeicoli();
+        List<Veicolo> ordinatiPerAnno = garageMisto.tuttiVeicoli();
         Collections.sort(ordinatiPerAnno);
         ordinatiPerAnno.forEach(System.out::println);
 
         System.out.println("\nCerco moto con cilindrata maggiore di 600");
-        garage.cercaVeicoli(
+        garageMisto.cercaVeicoli(
                 veicolo -> veicolo instanceof Moto && ((Moto)veicolo).getCilindrata() > 600
         ).forEach(System.out::println);
 
-        System.out.println("\nLe tasse totali sono: " + garage.tasseTotali());
+        System.out.println("\nLe tasse totali sono: " + garageMisto.tasseTotali());
     }
 }
--- src/com/github/giovannidiguida/gestionegarage/Garage.java
+++ src/com/github/giovannidiguida/gestionegarage/Garage.java
@@ -10,12 +10,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.function.Predicate;
 
-public class Garage {
+public class Garage<T extends Veicolo> {
     //vedo che l'es fatto generare male in python chiede un dizionario, quindi cambiamo in hashmap
-    private HashMap<String, Veicolo> mappaVeicoli = new HashMap<>();
+    private HashMap<String, T> mappaVeicoli = new HashMap<>();
 
     //aggiunta eccezione come richiesto da esercizio, anche se in questo caso si poteva evitare era meglio con il boolean
-    public void aggiungiVeicolo(Veicolo veicolo) {
+    public void aggiungiVeicolo(T veicolo) {
         if (mappaVeicoli.containsKey(veicolo.getTarga())) {
             throw new VeicoloDuplicatoException(veicolo);
         }
@@ -31,7 +31,7 @@ public class Garage {
 
     public double tasseTotali() {
         double totaleTasse = 0;
-        for (Veicolo value : mappaVeicoli.values()) {
+        for (T value : mappaVeicoli.values()) {
             totaleTasse += value.calcolaTassa();
         }
         return totaleTasse;
@@ -40,11 +40,11 @@ public class Garage {
     //probabilmente ti chiedi come mai non fai return diretto dei values?
     //lo faccio perché values non ritorna una copia, ma l'implementazione reale che c'è nella mappa
     //una modifica potrebbe rompere il sistema, come anche no, ma per sicurezza faccio una copia
-    public List<Veicolo> tuttiVeicoli() {
+    public List<T> tuttiVeicoli() {
         return new ArrayList<>(mappaVeicoli.values());
     }
 
-    public List<Veicolo> cercaVeicoli(Predicate<? super Veicolo> filtro) {
+    public List<T> cercaVeicoli(Predicate<? super T> filtro) {
         return mappaVeicoli.values()
                 .stream()
                 .filter(filtro)
@@ -52,7 +52,7 @@ public class Garage {
     }
 
     //todo ancora da implementare
-    public List<Veicolo> veicoliOrdinati(Comparator<Veicolo> comparator) {
+    public List<T> veicoliOrdinati(Comparator<T> comparator) {
         return mappaVeicoli.values()
                 .stream()
                 .sorted(comparator)
