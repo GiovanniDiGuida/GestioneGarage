276784a3a7107d0861cbc47e4255ac00be61b71c
RedFox197 <190109902+RedFox197@users.noreply.github.com>
2025-01-13 00:12:44 +0100

    rifatta classe App

--- src/com/github/giovannidiguida/gestionegarage/App.java
+++ src/com/github/giovannidiguida/gestionegarage/App.java
@@ -4,33 +4,31 @@ import com.github.giovannidiguida.gestionegarage.veicolo.Auto;
 import com.github.giovannidiguida.gestionegarage.veicolo.Moto;
 import com.github.giovannidiguida.gestionegarage.veicolo.Veicolo;
 
+import java.util.Collections;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Benvenuto nel Garage");
+
         Garage garage = new Garage();
+        garage.aggiungiVeicolo(new Moto("Modello Pippo", "Audi", "ABC123", 1900, 3000, 800));
+        garage.aggiungiVeicolo(new Moto("Modello Luca", "Idua", "CBA321", 1950, 4000, 100));
+        garage.aggiungiVeicolo(new Moto("Modello Bimbi", "Peppe", "Chicco", 2012, 4000, 610));
+        garage.aggiungiVeicolo(new Auto("Modello Catalin", "Dacia", "FRE123", 1998, 100, 4));
+        garage.aggiungiVeicolo(new Auto("Modello Giovanni", "Rubare", "IoRubo123", 1995, 100, 5));
 
+        //potevo usare anche il metodo garage.veicoliOrdinati, non lo uso solo per mostrare l'implementazione del Comparable<Veicolo>
+        System.out.println("\nOrdinati per anno");
+        List<Veicolo> ordinatiPerAnno = garage.tuttiVeicoli();
+        Collections.sort(ordinatiPerAnno);
+        ordinatiPerAnno.forEach(System.out::println);
 
-        Moto moto1 = new Moto("Modello Pippo", "Audi", "ABC123", 1900, 3000, 100);
-        Moto moto2 = new Moto("Modello Luca", "Idua", "CBA321", 1950, 4000, 100);
+        System.out.println("\nCerco moto con cilindrata maggiore di 600");
+        garage.cercaVeicoli(
+                veicolo -> veicolo instanceof Moto && ((Moto)veicolo).getCilindrata() > 600
+        ).forEach(System.out::println);
 
-        Auto auto1 = new Auto("Modello Catalin", "Dacia", "FRE123", 1998, 100, 4);
-        Auto auto2 = new Auto("Modello Giovanni", "Rubare", "IoRubo123", 1995, 100, 5);
-
-        garage.aggiungiVeicolo(moto1);
-        garage.aggiungiVeicolo(moto2);
-        garage.aggiungiVeicolo(auto1);
-        garage.aggiungiVeicolo(auto2);
-
-        for (Veicolo veicolo : garage.tuttiVeicoli()) { /*Metodo più veloce con foreach */
-            System.out.println(veicolo.getDescrizione());
-        }
-
-        /*garage.mappaVeicoli.forEach(Veicolo-> System.out.println(Veicolo.getDescrizione())); un altro modo per stampare e richiamare */
-
-        /*for (int i=0;i<garage.mappaVeicoli.size();i++){
-            System.out.println(garage.mappaVeicoli.get(i).getDescrizione());
-        } Metodo classico */
-
-        System.out.println("Le tasse totali sono: " + garage.tasseTotali());
+        System.out.println("\nLe tasse totali sono: " + garage.tasseTotali());
     }
 }
